6901f1856b3b2fd644fa7bc730b9b066b53c69bd
CrystalQuest112 <114012059+CrystalQuest112@users.noreply.github.com>
2022-09-27 11:26:51 +0200

    Updated Code
    + CSS Adjustments
    +HTML Adjustments
    +JS Adjustments

--- script.js
+++ script.js
@@ -1,32 +1,78 @@
-callTime ();
+ callTime();
+      function callTime() {
+        let currentTime = new Date();
+        console.log(currentTime.getDate());
+        let day = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ];
 
-function callTime() {
-let currentTime = new Date(); {
+        let date = day[currentTime.getDay()];
+        let hours = currentTime.getHours();
+        let minutes = currentTime.getMinutes();
 
-console.log(currentTime.getDate());
-let days = [ "Sunday" , "Monday", "Tuesday","Wednesday", "Thursday" , "Friday" , "Saturday"]
+        let clock = document.querySelector("#time");
+        clock.innerHTML = `${date}  ${hours}:${minutes}`;
+      }
 
-let date = days[currentTime.getDay()];
-let hours =currentTime.getHours();
-let minutes = currentTime.getMinutes();
+      function pullWeather(response) {
+        console.log(response.data);
+        console.log(response.data.main.temp);
 
-let h3 = document.querySelector("h3");
-h3.innerHTML = `${date}  ${hours}:${minutes}`;
-}}
+        let degrees = document.querySelector("#temperature");
+        degrees.innerHTML = Math.round(response.data.main.temp);
 
+        let place = document.querySelector("#place");
+        place.innerHTML = response.data.name;
 
-function showWeather(response){
-console.log(response.data); 
-document.querySelector("h4").innerHTML = response.data.name;
-document.querySelector("#temp").innerHTML = Math.round(response.data.main.temp);
-}
+        let des = document.querySelector("#description");
+        des.innerHTML = response.data.weather[0].main;
 
-function citySearch(event) {
-event.preventDefault();
-let apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=de561b681be5798e182b6fa0a38a5264`; 
-let apikey="de561b681be5798e182b6fa0a38a5264";
-let city  = document.querySelector("#city-input").value; 
-console.log(apiWeather); 
+        let hum = document.querySelector("#humidity");
+        hum.innerHTML = "Humidity: " + response.data.main.humidity + "%";
 
-axios.get(apiWeather).then(showWeather);
-}
+        let wind = document.querySelector("#windspeed");
+        wind.innerHTML =
+          "Wind: " + Math.round(response.data.wind.speed) + "Km/H";
+
+        let icon = document.querySelector("#icon");
+        icon.setAttribute(
+          "src",
+          `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        );
+      }
+
+      function search(city) {
+        let apiKey = "de561b681be5798e182b6fa0a38a5264";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+        axios.get(apiUrl).then(pullWeather);
+      }
+
+      function getCity(event) {
+        event.preventDefault();
+        let cityType = document.querySelector("#find");
+        search(cityType.value);
+      }
+
+      let form = document.querySelector("#search-form");
+      form.addEventListener("submit", getCity);
+
+      search("Nairobi");
+
+      function showPosition(position) {
+        let h1 = document.querySelector("h1");
+        h1.innerHTML = `Your location is " + ${position.coords.latitude}+ "&" + ${position.coords.latitude}.`;
+        console.log(position.coords.latitude);
+        console.log(position.coords.latitude);
+      }
+
+      function getCurrentPosition() {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      }
+      let geoButton = document.querySelector("#coord");
+      geoButton.addEventListener("click", getCurrentPosition);
